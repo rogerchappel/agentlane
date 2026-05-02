de089136535f6afff4eea39b7c697e94b9514400
Roger Chappel <miscanalysis@gmail.com>
2026-05-02 18:38:02 +1000

    feat: export public planning api

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { createPlan, buildLanes } from './plan-builder.js';
+export { renderPlanMarkdown } from './markdown.js';
+export { renderPlanJson } from './json.js';
+export { detectRepoFacts } from './detect.js';
+export type { Lane, LaneKind, PlanOptions, PlanResult, RepoFacts } from './types.js';
